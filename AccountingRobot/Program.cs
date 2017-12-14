0da9cc5c368c65cd46105c7ec2f6bea0009ab21a
Per Ivar Nerseth <perivar@nerseth.com>
2017-12-15 00:33:06 +0100

    Added Paypal and Stripe methods

--- AccountingRobot/Program.cs
+++ AccountingRobot/Program.cs
@@ -7,13 +7,34 @@ namespace AccountingRobot
     {
         static void Main(string[] args)
         {
+            // get paypal configuration parameters
+            string payPalApiUsername = ConfigurationManager.AppSettings["PayPalApiUsername"];
+            string payPalApiPassword = ConfigurationManager.AppSettings["PayPalApiPassword"];
+            string payPalApiSignature = ConfigurationManager.AppSettings["PayPalApiSignature"];
+            var paypalTransactions = Paypal.GetTransactions(payPalApiUsername, payPalApiPassword, payPalApiSignature);
+            foreach (var paypalTransaction in paypalTransactions)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", paypalTransaction.Timestamp, paypalTransaction.GrossAmount.value, paypalTransaction.FeeAmount.value, paypalTransaction.PayerDisplayName);
+            }
+
             /*
+            // get stripe configuration parameters
+            string stripeApiKey = ConfigurationManager.AppSettings["StripeApiKey"];
+            var stripeTransactions = Stripe.GetTransactions(stripeApiKey);
+            foreach (var stripeTransaction in stripeTransactions)
+            {
+                decimal amount = (decimal)stripeTransaction.Amount / 100;
+                decimal fee = (decimal)stripeTransaction.Fee / 100;
+                Console.WriteLine("{0:yyyy.MM.dd} {1:N} {2:N} {3} {4} {5} {6}", stripeTransaction.Created, amount, fee, stripeTransaction.Currency, stripeTransaction.Description, stripeTransaction.Type, stripeTransaction.Status);
+            }
+
+            
             var oberloOrders = Oberlo.ReadOrders(@"C:\Users\pnerseth\Amazon Drive\Documents\Private\wazalo\regnskap\Oberlo Orders 2017-01-01-2017-12-04.xlsx");
             foreach (var oberloOrder in oberloOrders)
             {
                 Console.WriteLine("{0}", oberloOrder);
             }
-            */
+            
 
             var skandiabankenTransactions = Skandiabanken.ReadTransactions(@"C:\Users\pnerseth\Amazon Drive\Documents\Private\wazalo\regnskap\97132735232_2017_01_01-2017_12_10.xlsx");
             foreach (var skandiabankenTransaction in skandiabankenTransactions)
@@ -21,7 +42,7 @@ namespace AccountingRobot
                 Console.WriteLine("{0}", skandiabankenTransaction.GuessAccountType());
             }
 
-            /*
+            
             var aliExpressOrders = AliExpress.ReadOrders(@"C:\Users\pnerseth\Amazon Drive\Documents\Private\wazalo\regnskap\AliExpressOrders-2017-12-10_00-59.csv");
             foreach (var aliExpressOrder in aliExpressOrders)
             {
---
+++ AccountingRobot/Stripe.cs
@@ -0,0 +1,49 @@
+﻿using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingRobot
+{
+    public static class Stripe
+    {
+        public static List<StripeBalanceTransaction> GetTransactions(string stripeApiKey)
+        {
+            StripeConfiguration.SetApiKey(stripeApiKey);
+            var balanceService = new StripeBalanceService();            
+            var allBalanceTransactions = new List<StripeBalanceTransaction>();
+
+            var lastId = String.Empty;
+
+            int MAX_PAGINATION = 100;
+            int itemsExpected = MAX_PAGINATION;
+            while (itemsExpected == MAX_PAGINATION)
+            {
+                IEnumerable<StripeBalanceTransaction> balanceTransactions = null;
+                if (String.IsNullOrEmpty(lastId))
+                {
+                    balanceTransactions = balanceService.List(
+                    new StripeBalanceTransactionListOptions() { Limit = MAX_PAGINATION });
+                    itemsExpected = balanceTransactions.Count();
+                }
+                else
+                {
+                    balanceTransactions = balanceService.List(
+                    new StripeBalanceTransactionListOptions()
+                    {
+                        Limit = MAX_PAGINATION,
+                        StartingAfter = lastId
+                    });
+                    itemsExpected = balanceTransactions.Count();
+                }
+
+                allBalanceTransactions.AddRange(balanceTransactions);
+                lastId = balanceTransactions.LastOrDefault().Id;
+            }
+
+            return allBalanceTransactions;
+        }
+    }
+}
